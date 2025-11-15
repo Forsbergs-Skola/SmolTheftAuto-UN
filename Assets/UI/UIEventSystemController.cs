5eb89471c7a277572b531eb251b4af00443821a8
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-11-15 14:11:44 +0100

    Prefab-ify the CanvasManager

--- Assets/UI/UIEventSystemController.cs
+++ Assets/UI/UIEventSystemController.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using GameTools;
 
 public class UIEventSystemController : MonoBehaviour
 {
     private void Awake()
     {
+        gameObject.name = "SingletonEventSystem";
+        
+       foreach(GameObject rootObj in SceneManager.GetActiveScene().GetRootGameObjects())
+        {
+            if (gameObject.name == "EventSystem") { Destroy(gameObject); }
+            if (gameObject.name == "Canvas") { Destroy(gameObject); }
+        }
+
+
         if (GameObject.FindGameObjectsWithTag(Constants.Tags.UI_EVENT_SYSTEM).Length > 0) { Destroy(gameObject); }
         tag = Constants.Tags.UI_EVENT_SYSTEM;
         DontDestroyOnLoad(gameObject);
