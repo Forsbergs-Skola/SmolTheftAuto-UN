cd7bb3424170913b49fa2b48ab5e56fc64c2221f
danglenzig <99054411+danglenzig@users.noreply.github.com>
2025-11-17 12:17:02 +0100

    Started on the inventory UI

--- Assets/Scripts/UIScripts/PauseCanvas/PauseCanvas.cs
+++ Assets/Scripts/UIScripts/PauseCanvas/PauseCanvas.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PauseCanvas : MonoBehaviour, ICanvasable
 {
+
+    private enum EnumPausePanel
+    {
+        INVENTORY,
+        QUESTS,
+    }
+
+    [SerializeField] private Button switchViewButton;
+    [SerializeField] private TMP_Text switchViewButtonText;
+    [SerializeField] private InventoryPanel inventoryPanel;
+    [SerializeField] private QuestPanel questPanel;
+
+    private EnumPausePanel currentPanel;
     private bool _isVisible = false;
     private bool isVisible
     {
@@ -12,12 +27,17 @@ public class PauseCanvas : MonoBehaviour, ICanvasable
             {
                 _isVisible = value;
                 gameObject.SetActive(_isVisible);
+                HandleVisiblityChanged();
             }
         }
     }
     private void Awake()
     {
         isVisible = gameObject.activeInHierarchy;
+    }
+    private void Start()
+    {
+        DisplayPanel(EnumPausePanel.QUESTS);
     }
 
     public EnumCanvasName CanvasName()
@@ -36,4 +56,44 @@ public class PauseCanvas : MonoBehaviour, ICanvasable
     {
         isVisible = val;
     }
+
+    private void DisplayPanel(EnumPausePanel panel)
+    {
+        switch (panel)
+        {
+            case EnumPausePanel.INVENTORY:
+                questPanel.gameObject.SetActive(false);
+                inventoryPanel.gameObject.SetActive(true);
+                switchViewButtonText.text = "View Quest Log";
+                currentPanel = EnumPausePanel.INVENTORY;
+                break;
+            case EnumPausePanel.QUESTS:
+                questPanel.gameObject.SetActive(true);
+                inventoryPanel.gameObject.SetActive(false);
+                switchViewButtonText.text = "View Inventory";
+                currentPanel = EnumPausePanel.QUESTS;
+                break;
+            default: return;
+        }
+    }
+
+    public void HandleOnSwitchViewButtonPressed()
+    {
+        switch (currentPanel)
+        {
+            case EnumPausePanel.INVENTORY:
+                DisplayPanel(EnumPausePanel.QUESTS);
+                break;
+            case EnumPausePanel.QUESTS:
+                DisplayPanel(EnumPausePanel.INVENTORY);
+                break;
+            default: return;
+        }
+    }
+
+    private void HandleVisiblityChanged()
+    {
+        DisplayPanel(EnumPausePanel.QUESTS);
+    }
+
 }
